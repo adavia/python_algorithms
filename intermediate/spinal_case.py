e7a3130621d572e0be0b7a581d5af81a7502484a
adavia <andresi.davia@gmail.com>
2019-09-11 21:20:49 -0500

    Add more algorithms

---
+++ intermediate/spinal_case.py
@@ -0,0 +1,12 @@
+import re
+
+def spinal_case(s):
+    match = re.split(r'(?=[A-Z])|_|-|\s', s.lower())
+    return '-'.join(filter(lambda x: x != '', match))
+    
+print(spinal_case('This-Is-Spinal-Tap'))
+print(spinal_case('This_Is_Spinal_Tap'))
+print(spinal_case('This Is Spinal Tap'))
+print(spinal_case('ThisIsSpinalTap'))
+print(spinal_case('Teletubbies say Eh-oh'))
+print(spinal_case('AllThe-small Things'))
